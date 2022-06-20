c821fece98c22fb2884d18f26f68d7438e547bb8
Mohamed Hesham <mohamed.hesham@emeint.net>
2022-06-20 19:58:01 +0200

    Add new profiles

--- src/test/java/com/bluesoft/scriptgenerator/ScriptGeneratorApplicationTests.java
+++ src/test/java/com/bluesoft/scriptgenerator/ScriptGeneratorApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ClothingStoreManagementApplicationTests {
+class ScriptGeneratorApplicationTests {
 
 	@Test
 	void contextLoads() {
